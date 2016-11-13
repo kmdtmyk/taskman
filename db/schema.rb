e624d7cff5ea9090541b912070788eb9e2ce4dd1
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-11-13 20:35:25 +0900

    add tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161110115953) do
+ActiveRecord::Schema.define(version: 20161112092842) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,14 @@ ActiveRecord::Schema.define(version: 20161110115953) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_tasks_on_project_id", using: :btree
+  end
+
+  add_foreign_key "tasks", "projects"
 end
