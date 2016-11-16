1bdc4f7488edc0df1b863b7eb3ebb9ccbc9d82fa
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-11-16 22:40:44 +0900

    [task] add new

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,35 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show]
+  before_action :set_project, only: [:new, :create]
+
+  def new
+    @task = @project.tasks.build
+  end
 
   def show
   end
 
+  def create
+    @task = @project.tasks.build(task_params)
+    if @task.save
+      redirect_to @project, notice: 'Taks was successfully created.'
+    else
+      render :new
+    end
+  end
+
   private
+
+    def set_project
+      @project = Project.find_by(name: params[:project_name])
+    end
+
     def set_task
       @task = Task.find(params[:id])
+    end
+
+    def task_params
+      params.require(:task).permit(:title, :body)
     end
 
 end
