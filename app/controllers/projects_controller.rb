22d3a2a6daa232ac964e8a9c4680365a2744f2a0
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-11-27 20:21:07 +0900

    [task] add search form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,6 +10,8 @@ class ProjectsController < ApplicationController
   # GET /projects/1
   # GET /projects/1.json
   def show
+    @q = params[:q]
+    @tasks = Task.where('title LIKE ?', "%#{@q}%")
   end
 
   # GET /projects/new
