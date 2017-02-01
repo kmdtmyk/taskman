3d7ecfd87f6fc5ad94abc1d01f5a1a243d0eb920
kmdtmyk <winback_banderas@yahoo.co.jp>
2017-02-01 22:21:29 +0900

    install redcarpet

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'friendly_id', '~> 5.1'
 
 gem 'kaminari', '~> 0.17'
 
+gem 'redcarpet', '~> 3.4'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+  require 'redcarpet'
+
+  def markdown(text)
+    render_options = {
+      filter_html: true,
+    }
+    renderer = Redcarpet::Render::HTML.new(render_options)
+    extensions = {
+      autolink: true,
+      tables: true,
+    }
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+    markdown.render(text).html_safe
+  end
+
 end
